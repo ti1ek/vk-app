564341373ae2921e02118a796bfea3dc6ea39357
tkoszhanov <tilek.koszhanov@mail.ru>
2023-01-10 22:37:22 +0500

    First commit

---
+++ VK/VKApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKApp.swift
+//  VK
+//
+//  Created by Tilek Koszhanov on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct VKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
