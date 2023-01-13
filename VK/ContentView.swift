5d3558bcc9eee7c252cab5d37d1a05daf5495f67
tkoszhanov <tilek.koszhanov@mail.ru>
2023-01-13 18:43:53 +0500

    Friend and Group cells are set

--- VK/ContentView.swift
+++ VK/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
